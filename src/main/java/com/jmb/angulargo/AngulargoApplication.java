83ccdc88fcaef5b23309b2f749962fc4a3b8e287
Jules-Martial BEYAM <bmjmartial@gmail.com>
2018-04-18 00:10:25 +0200

    first commit

---
+++ src/main/java/com/jmb/angulargo/AngulargoApplication.java
@@ -0,0 +1,15 @@
+package com.jmb.angulargo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class AngulargoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngulargoApplication.class, args);
+	}
+}
